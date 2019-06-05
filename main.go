26c52688cef8bd705dd4a1dfade6ec50c0f48023
earthsittipong <earth32977@gmail.com>
2019-06-05 17:50:34 +0700

    first commit

--- main.go
+++ main.go
@@ -1,14 +1,19 @@
 ﻿package main
 
-import ("github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
 "strconv")
 
 func main() {
 	r := gin.Default()
 	r.GET("/lab3", func(c *gin.Context) {
-		point := c.DefaultQuery("point", "0")
-		p, _ := strconv.Atoi(point)
-		if p > 90 {
+		point := c.Query("point")
+		p, err := strconv.Atoi(point)
+		if err != nil {
+			fmt.Println("not grade",err)
+			c.String(200,"not grade")
+		} else if p > 90 {
 			c.JSON(200, gin.H{
 				"grade": "A",
 				"point": point,
@@ -33,6 +38,12 @@ func main() {
 				"grade": "F",
 				"point": point,
 			})
+		} else  {
+			c.JSON(200, gin.H{
+				"grade": "not grade",
+				"point": point,
+			})
+
 		}
 	})
 	r.Run(":8000")
